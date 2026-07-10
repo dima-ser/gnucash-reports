726caaac3debb1ed87dd7bea6e321671f3433119
dima-ser <dserhiychuk@gmail.com>
2026-07-10 16:11:21 -0700

    Two side-by-side years of cash flows now work

--- GnuCashReports/Pages/Cashflow.cshtml.cs
+++ GnuCashReports/Pages/Cashflow.cshtml.cs
@@ -6,16 +6,34 @@ using Microsoft.Extensions.Options;
 
 namespace GnuCashReports.Pages
 {
+    public class CashFlowCombinedItem
+    {
+        public string Category { get; set; } = String.Empty;
+        public decimal Amount1 { get; set; }
+        public decimal Amount2 { get; set; }
+
+        public bool Equals(CashFlowCombinedItem? other)
+        {
+            if (other is null) return false;
+            return string.Equals(Category, other.Category, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as CashFlowCombinedItem);
+
+        public override int GetHashCode() =>
+            Category?.GetHashCode(StringComparison.OrdinalIgnoreCase) ?? 0;
+    }
 
     public class CashflowModel : PageModel
     {
         public enum CashFlowType  {Inflow, Outflow}
+        
         private readonly DatabaseService _dbService;
 
         private readonly AppSettings _appSettings;
-        public Dictionary<string, decimal> Inflows1 = new Dictionary<string, decimal>();
-        public Dictionary<string, decimal> Outflows1 = new Dictionary<string, decimal>();
 
+        public List<CashFlowCombinedItem> Inflows = new List<CashFlowCombinedItem>();
+        public List<CashFlowCombinedItem> Outflows = new List<CashFlowCombinedItem>();
         public CashflowModel(DatabaseService dbService, IOptions<AppSettings> appSettings)
         {
             _dbService = dbService;
@@ -26,21 +44,68 @@ namespace GnuCashReports.Pages
         {
             if (String.IsNullOrWhiteSpace(_appSettings.CashFlowSettings?.ParentCashAccount))
                 throw new Exception("Missing configuration \"ParentCashAccount\"");
+            Dictionary<string, decimal> inflows1 = new Dictionary<string, decimal>();
+            Dictionary<string, decimal> outflows1 = new Dictionary<string, decimal>();
+            Dictionary<string, decimal> inflows2 = new Dictionary<string, decimal>();
+            Dictionary<string, decimal> outflows2 = new Dictionary<string, decimal>();
             List<CashFlowItem> cashFlowItems1  = await _dbService.GetCashFlowStatement(
                 _appSettings.CashFlowSettings.ParentCashAccount, 
                 new DateTime(DateTime.Now.Year, 1, 1), 
                 new DateTime(DateTime.Now.Year + 1, 1, 1));
-            Inflows1 = RewriteCashflowCategories(
+            inflows1 = RewriteCashflowCategories(
                 cashFlowItems1.Where(c => c.Inflow > 0).ToList(), 
                 _appSettings.CashFlowSettings.InflowCategories, 
                 CashFlowType.Inflow);
-            Outflows1 = RewriteCashflowCategories(
+            outflows1 = RewriteCashflowCategories(
                 cashFlowItems1.Where(c => c.Outflow > 0).ToList(), 
                 _appSettings.CashFlowSettings.OutflowCategories, 
                 CashFlowType.Outflow);
+            List<CashFlowItem> cashFlowItems2  = await _dbService.GetCashFlowStatement(
+                _appSettings.CashFlowSettings.ParentCashAccount, 
+                new DateTime(DateTime.Now.Year - 1, 1, 1), 
+                new DateTime(DateTime.Now.Year, 1, 1));
+            inflows2 = RewriteCashflowCategories(
+                cashFlowItems2.Where(c => c.Inflow > 0).ToList(), 
+                _appSettings.CashFlowSettings.InflowCategories, 
+                CashFlowType.Inflow);
+            outflows2 = RewriteCashflowCategories(
+                cashFlowItems2.Where(c => c.Outflow > 0).ToList(), 
+                _appSettings.CashFlowSettings.OutflowCategories, 
+                CashFlowType.Outflow);
 
+            Inflows = CombineCashFlows(inflows1, inflows2);
+            Outflows = CombineCashFlows(outflows1, outflows2);
 
+        }
 
+        public List<CashFlowCombinedItem> CombineCashFlows(Dictionary<string, decimal> cashFlows1, Dictionary<string, decimal> cashFlows2)
+        {
+            List<CashFlowCombinedItem> combinedItems = new List<CashFlowCombinedItem>();
+            foreach(var item1 in cashFlows1)
+            {
+                bool foundMatch = false;
+                foreach(var item2 in cashFlows2)
+                {
+                    if (item1.Key == item2.Key)
+                    {
+                        foundMatch = true;
+                        combinedItems.Add(new CashFlowCombinedItem{ Category = item1.Key, Amount1 = item1.Value, Amount2 = item2.Value});
+                        break;
+                    }
+                }
+                if (!foundMatch)
+                    combinedItems.Add(new CashFlowCombinedItem{ Category = item1.Key, Amount1 = item1.Value, Amount2 = 0});
+            }
+            // add remaining items from cashFlows2 that didn't have a match
+            foreach(var item2 in cashFlows2)
+            {
+                var combinedItem = new CashFlowCombinedItem { Category = item2.Key, Amount1 = 0, Amount2 = item2.Value };
+                if (!combinedItems.Contains(combinedItem))
+                {
+                    combinedItems.Add(combinedItem);
+                }
+            }
+            return combinedItems;
         }
 
         public Dictionary<string, decimal> RewriteCashflowCategories(
