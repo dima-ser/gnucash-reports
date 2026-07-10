b6065812897a2923c498078715af936fe6208221
dima-ser <dserhiychuk@gmail.com>
2026-07-10 15:18:27 -0700

    Refactoring

--- GnuCashReports/Pages/Cashflow.cshtml.cs
+++ GnuCashReports/Pages/Cashflow.cshtml.cs
@@ -9,11 +9,12 @@ namespace GnuCashReports.Pages
 
     public class CashflowModel : PageModel
     {
+        public enum CashFlowType  {Inflow, Outflow}
         private readonly DatabaseService _dbService;
 
         private readonly AppSettings _appSettings;
-        public Dictionary<string, decimal> InflowsYtd = new Dictionary<string, decimal>();
-        public Dictionary<string, decimal> OutflowsYtd = new Dictionary<string, decimal>();
+        public Dictionary<string, decimal> Inflows1 = new Dictionary<string, decimal>();
+        public Dictionary<string, decimal> Outflows1 = new Dictionary<string, decimal>();
 
         public CashflowModel(DatabaseService dbService, IOptions<AppSettings> appSettings)
         {
@@ -25,55 +26,52 @@ namespace GnuCashReports.Pages
         {
             if (String.IsNullOrWhiteSpace(_appSettings.CashFlowSettings?.ParentCashAccount))
                 throw new Exception("Missing configuration \"ParentCashAccount\"");
-            List<CashFlowItem> cashFlowItemsYtd  = await _dbService.GetCashFlowStatement(
+            List<CashFlowItem> cashFlowItems1  = await _dbService.GetCashFlowStatement(
                 _appSettings.CashFlowSettings.ParentCashAccount, 
                 new DateTime(DateTime.Now.Year, 1, 1), 
                 new DateTime(DateTime.Now.Year + 1, 1, 1));
-            
-            foreach (var item in cashFlowItemsYtd.Where(c => c.Inflow > 0))
+            Inflows1 = RewriteCashflowCategories(
+                cashFlowItems1.Where(c => c.Inflow > 0).ToList(), 
+                _appSettings.CashFlowSettings.InflowCategories, 
+                CashFlowType.Inflow);
+            Outflows1 = RewriteCashflowCategories(
+                cashFlowItems1.Where(c => c.Outflow > 0).ToList(), 
+                _appSettings.CashFlowSettings.OutflowCategories, 
+                CashFlowType.Outflow);
+
+
+
+        }
+
+        public Dictionary<string, decimal> RewriteCashflowCategories(
+            List<CashFlowItem> inputList, 
+            Dictionary<string, List<string>>? categories, 
+            CashFlowType type)
+        {
+            Dictionary<string, decimal> cashFlows = new Dictionary<string, decimal>();
+            foreach (var item in inputList)
             {
-                bool isOverridden = false;
-                if (_appSettings.CashFlowSettings.InflowCategories != null)
+                bool isRewritten = false;
+                if (categories != null)
                 {
-                    foreach (var category in _appSettings.CashFlowSettings.InflowCategories)
+                    foreach (var category in categories)
                     {
                         foreach (var pattern in category.Value){
                             if (item.AccountPath.StartsWith(pattern))
                             {
-                                decimal total = InflowsYtd.ContainsKey(category.Key) ? InflowsYtd[category.Key] : 0;
-                                total += item.Inflow;
-                                InflowsYtd.Remove(category.Key);
-                                InflowsYtd.Add(category.Key, total);
-                                isOverridden = true;
+                                decimal total = cashFlows.ContainsKey(category.Key) ? cashFlows[category.Key] : 0;
+                                total += type == CashFlowType.Inflow ? item.Inflow : item.Outflow;
+                                cashFlows.Remove(category.Key);
+                                cashFlows.Add(category.Key, total);
+                                isRewritten = true;
                             }
                         }
                     }
                 }
-                if (!isOverridden)
-                    InflowsYtd.Add(item.AccountPath, item.Inflow);
+                if (!isRewritten)
+                    cashFlows.Add(item.AccountPath, type == CashFlowType.Inflow ? item.Inflow : item.Outflow);
             }
-            foreach (var item in cashFlowItemsYtd.Where(c => c.Outflow > 0))
-            {
-                bool isOverridden = false;
-                if (_appSettings.CashFlowSettings.OutflowCategories != null)
-                {
-                    foreach (var category in _appSettings.CashFlowSettings.OutflowCategories)
-                    {
-                        foreach (var pattern in category.Value){
-                            if (item.AccountPath.StartsWith(pattern))
-                            {
-                                decimal total = OutflowsYtd.ContainsKey(category.Key) ? OutflowsYtd[category.Key] : 0;
-                                total += item.Outflow;
-                                OutflowsYtd.Remove(category.Key);
-                                OutflowsYtd.Add(category.Key, total);
-                                isOverridden = true;
-                            }
-                        }
-                    }
-                }
-                if (!isOverridden)
-                    OutflowsYtd.Add(item.AccountPath, item.Outflow);
-            }
+            return cashFlows;
         }
     }
 
