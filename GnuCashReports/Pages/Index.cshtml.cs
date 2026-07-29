aaa8bf9ede570d2dc8e2125e300b5863bc9f290c
dima-ser <dserhiychuk@gmail.com>
2026-07-29 11:41:35 -0700

    Allow adding reports to home page dashboard

--- GnuCashReports/Pages/Index.cshtml.cs
+++ GnuCashReports/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GnuCashReports.Models;
+using Microsoft.Extensions.Options;
 
 namespace GnuCashReports.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<List<string>>? DashboardLayout {get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IOptions<AppSettings> appSettings)
         {
             _logger = logger;
+
+            DashboardLayout = appSettings.Value.DashboardLayout;
+            
         }
 
         public void OnGet()
         {
-            //Response.Redirect("profitloss");
+
         }
     }
 }
