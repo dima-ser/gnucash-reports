0892afd688b79f0d04f1831b1e03647e45bbfec5
dima-ser <dserhiychuk@gmail.com>
2026-08-09 14:46:03 -0700

    Add mc to average expenses

--- GnuCashReports/Services/DatabaseService.cs
+++ GnuCashReports/Services/DatabaseService.cs
@@ -145,38 +145,72 @@ FROM pl_level2";
                 int numYears = _appSettings.FISettings.AverageExpensesYearsLookback;
                 command.Parameters.Add(new SqliteParameter("@numYears", numYears));
                 DateTime startDate = new DateTime(DateTime.Now.Year - numYears, 1, 1);
-                DateTime endDate = new DateTime(DateTime.Now.Year, 1, 1);
+                DateTime endDate = new DateTime(DateTime.Now.Year - 1, 12, 31);
                 command.Parameters.Add(new SqliteParameter("@startDate", startDate));
                 command.Parameters.Add(new SqliteParameter("@endDate", endDate));
+                command.Parameters.Add(new SqliteParameter("@reportCurrency", _appSettings.ReportCurrency));
                 //command.Parameters.Add(new SqliteParameter("@ignorePattern", 
                 //    !String.IsNullOrWhiteSpace(_appSettings.ClosingEntriesPattern) ? _appSettings.ClosingEntriesPattern : DBNull.Value));
                 command.CommandText = @"
             WITH RECURSIVE
-root_guid as (select guid from accounts where parent_guid is NULL and name='Root Account'),
+root_guid as (select root_account_guid as guid from books limit 1),
 root_expense_guid as (select guid from accounts where account_type='EXPENSE' and parent_guid=(select guid from root_guid)),
 account_tree AS (
-    SELECT a.guid, a.name, a.parent_guid, a.account_type,
+    SELECT a.guid, a.name, a.parent_guid, a.account_type, a.commodity_guid,
            a.guid AS level2_guid, a.name AS level2_name
     FROM accounts a
     WHERE a.parent_guid=(select guid from root_expense_guid)
 
     UNION ALL
 
-    SELECT a.guid, a.name, a.parent_guid, a.account_type,
+    SELECT a.guid, a.name, a.parent_guid, a.account_type, a.commodity_guid,
            at.level2_guid, at.level2_name
     FROM accounts a
     JOIN account_tree at ON a.parent_guid = at.guid
 ),
 closing_txguids as (select obj_guid from slots where name='book_closing'),
+latest_prices AS (
+    SELECT p.commodity_guid, p.currency_guid, MAX(p.date) AS latest_date
+    FROM prices p 
+    WHERE DATE(p.date) <= DATE(@endDate)  
+    GROUP BY p.commodity_guid, p.currency_guid
+),
+primary_prices as (
+SELECT p.commodity_guid, p.currency_guid, c.mnemonic, c.namespace, p.value_num * 1.0 / p.value_denom AS price
+    FROM prices p 
+    JOIN latest_prices lp ON p.commodity_guid = lp.commodity_guid AND p.currency_guid=lp.currency_guid AND p.date = lp.latest_date
+	JOIN commodities c on p.currency_guid=c.guid),
+inverse_prices as (
+ SELECT p.currency_guid as commodity_guid, p.commodity_guid as currency_guid, c.mnemonic, c.namespace, p.value_denom * 1.0 / p.value_num AS price
+    FROM prices p
+    JOIN latest_prices lp ON p.commodity_guid = lp.commodity_guid AND p.currency_guid=lp.currency_guid AND p.date = lp.latest_date
+	JOIN commodities c on p.commodity_guid=c.guid),
+all_prices as (
+SELECT * from primary_prices
+UNION
+SELECT * from inverse_prices
+UNION -- second order prices	
+SELECT p.commodity_guid, i.currency_guid, i.mnemonic, i.namespace, p.price * i.price as price 
+	FROM primary_prices p 
+	JOIN inverse_prices i on p.currency_guid=i.commodity_guid),
 pl_level2_ytd AS (
     SELECT at.level2_guid, at.level2_name AS account_name,
            at.account_type,
-           SUM(s.value_num * 1.0 / s.value_denom) AS total_amount
+           SUM(
+            CASE 
+                WHEN c.mnemonic = @reportCurrency and c.namespace='CURRENCY' THEN
+					s.quantity_num * 1.0 / s.quantity_denom
+                ELSE
+					s.quantity_num * 1.0 / s.quantity_denom * IFNULL(pl.price, 0)  
+            END
+        ) AS total_amount
     FROM splits s
     JOIN transactions t ON s.tx_guid = t.guid
     JOIN account_tree at ON s.account_guid = at.guid
-    WHERE t.post_date BETWEEN @startDate AND @endDate
-           AND t.guid not in (select obj_guid from closing_txguids)
+	LEFT JOIN commodities c ON at.commodity_guid = c.guid
+	LEFT JOIN all_prices pl ON at.commodity_guid = pl.commodity_guid and pl.mnemonic=@reportCurrency and pl.namespace='CURRENCY'
+    WHERE DATE(t.post_date) >= DATE(@startDate) AND DATE(t.post_date) <= DATE(@endDate) AND 
+		t.guid not in (select obj_guid from closing_txguids)
     GROUP BY at.level2_guid, at.level2_name, at.account_type
 )
 select sum(total_amount)/@numYears as AverageAnnualExpenses from pl_level2_ytd";
